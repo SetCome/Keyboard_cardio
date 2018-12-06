f2a2fdbe13a6dab168e65ac52564673655671ec4
SetCome <kamz-007@wp.pl>
2018-12-06 18:29:22 +0100

    js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //napisz funkcje która generuje losowa liczbę (zwraca liczbę całkowitą), zwraca jak argumenty przyjmuje 
 //min (minimalna wartość) oraz max (maxymalna liczba do wygenerowania)
 
-function losuj (min, max) {
-    return Math.floor(Math.random() * (max-min+1) + min);
-}
+function getRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
