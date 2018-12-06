0b38d42c30c70b15592d0771cc207c1c5a03fe47
SetCome <kamz-007@wp.pl>
2018-12-06 19:48:43 +0100

    js2

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //napisz funkcje która generuje losowa liczbę (zwraca liczbę całkowitą), zwraca jak argumenty przyjmuje 
 //min (minimalna wartość) oraz max (maxymalna liczba do wygenerowania)
 
-function getRandomNumber (min, max) {
+const buttonsKey = document.getElementsByClassName('key');
+let lastKey;
+function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+function getRandomKey() {
+    index = Math.floor(Math.random() * buttonsKey.length);
+    const key = buttonsKey[index]; 
+    if (key === lastKey){
+        console.log('the same key!!!');
+        return getRandomKey();
+    }
+    lastKey = key;
+    return key;
+};
+
+console.log(getRandomKey())
+
+
+// do domu napisz funkcje ktora losowa dodaje do klawisza klase fire-key
